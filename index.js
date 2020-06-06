e4cafb6b37f9d02698e5ecfe140842933ab64dd3
Felix Fernandez <fernandezfelix460.gmail.com>
2020-06-06 10:39:17 -0400

    added form event listener, added validation, created alert message on submission if fields were empty with js

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-alert()
+const searchForm = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
+
+//Form Event Listener
+searchForm.addEventListener('submit', e =>{
+    
+    // Get search term
+    const searchTerm = searchInput.value;
+    // Get sort by
+    const sortBy = document.querySelector('input[name]:checked').value;
+    // Get Limit
+    const limit = document.getElementById('limit').value;
+
+    // Validation Submission
+    if(searchTerm === '' || sortBy === '' || limit === ''){
+        showMessage('Please fill all required fields', 'alert-danger')
+    }
+
+    
+    e.preventDefault();
+});
+
+function showMessage(message, className){
+// Create div
+ const div = document.createElement('div');
+//  Add classes
+ div.className = `alert ${className}`;
+//  Add Text
+div.appendChild(document.createTextNode(message));
+// Get Parent 
+const searchContainer = document.getElementById('search-container');
+// Get search
+const search = document.getElementById('search');
+// Insert Message
+searchContainer.insertBefore(div, search);
+// Set time for alert to disappear
+setTimeout(()=>
+    document.querySelector('.alert').remove(), 2000
+)
+}
